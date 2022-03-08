41fa2cecae0aa86049ecb0c6dfc316f83c5ba124
Younus100 <67996526+Younus100@users.noreply.github.com>
2022-03-08 19:43:26 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import data from "./data.json";
+import "./styles.css";
+import Board from "react-trello";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react-trello demo</h1>
+      <Board
+        data={data}
+        draggable
+        editable
+        canAddLanes
+        addLaneTitle="Add Column"
+        addCardTitle="Add Item"
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
